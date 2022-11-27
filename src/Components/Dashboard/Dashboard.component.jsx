6b53c2236896789fc4b94a5bfb873ba1179fc048
mahyarfardin <mahyarfardinfar@gmail.com>
2022-11-27 22:11:58 +0330

    made dashboard responsive

--- src/Components/Dashboard/Dashboard.component.jsx
+++ src/Components/Dashboard/Dashboard.component.jsx
@@ -1,19 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Menu from "../menu/menu.component";
-import { BsPower } from "react-icons/bs";
 import Item from "../menu/Item.listItem";
 
 function Dashboard(props) {
+  const [hamMenu, setHamMenu] = useState(false);
   return (
-    <div className="hidden md:flex md:flex-col shadow-2xl shadow-gray w-1/5 h-screen">
-      <div className="w-full h-full flex-col justify-between text-center">
-        <img className="rounded-full" src="props" alt="profile picture" />
-        <h3>{props.name}</h3>
-        <Menu />
+    <>
+      <GiHamburgerMenu
+        onClick={() => setHamMenu((prev) => !prev)}
+        size={20}
+        className="text-gray lg:hidden m-4 hover:cursor-pointer"
+      />
+      {hamMenu && (
+        <div className="flex flex-col lg:hidden shadow-2xl shadow-gray w-2/3 h-full">
+          <div className="w-full h-full flex-col justify-between text-center">
+            <img className="rounded-full" src="props" alt="profile picture" />
+            <h3>{props.name}</h3>
+            <Menu classInfo={"flex flex-col justify-evenly "} />
+          </div>
+        </div>
+      )}
+
+      <div className="hidden lg:flex lg:flex-col shadow-2xl shadow-gray w-1/5 h-screen">
+        <div className="w-full h-full flex-col justify-between text-center">
+          <img className="rounded-full" src="props" alt="profile picture" />
+          <h3>{props.name}</h3>
+          <Menu classInfo={"flex flex-col justify-evenly "} />
+        </div>
       </div>
-      <Item first={<BsPower size={20}  className="text-gray"/>} second={<BsPower size={20}  className="text-gray"/>} clicked={false} text="logout"/>
-    </div>
+    </>
   );
 }
 
--- src/Components/menu/menu.component.jsx
+++ src/Components/menu/menu.component.jsx
@@ -10,12 +10,12 @@ import {
   BsCalendarWeek,
   BsGrid1X2Fill,
   BsGrid1X2,
+  BsPower,
 } from "react-icons/bs";
 import { RiBellFill, RiBellLine } from "react-icons/ri";
 import { AiOutlineSetting, AiTwotoneSetting } from "react-icons/ai";
 
-
-export default function Menu() {
+export default function Menu(props) {
   const [active, setActive] = useState({
     home: false,
     chat: true,
@@ -36,49 +36,81 @@ export default function Menu() {
 
   console.log(active);
   return (
-    <ul className="flex flex-col justify-evenly ">
+    <ul className={props.classInfo}>
       <Item
         first={<BsGrid1X2Fill size={20} className="text-blue" />}
         second={<BsGrid1X2 size={20} className="text-gray" />}
         text="home"
         clicked={active.home}
-        onclick={()=>setActive(()=>{return({...scheme,home:true})})}
+        onclick={() =>
+          setActive(() => {
+            return { ...scheme, home: true };
+          })
+        }
       />
       <Item
         first={<BsFillChatDotsFill size={20} className="text-blue" />}
         second={<BsChatDots size={20} className="text-gray" />}
         text="chat"
         clicked={active.chat}
-        onclick={()=>setActive(()=>{return({...scheme,chat:true})})}
+        onclick={() =>
+          setActive(() => {
+            return { ...scheme, chat: true };
+          })
+        }
       />
       <Item
         first={<BsPersonCheckFill size={20} className="text-blue" />}
         second={<BsPersonCheck size={20} className="text-gray" />}
         text="profile"
         clicked={active.profile}
-        onclick={()=>setActive(()=>{return({...scheme,profile:true})})}
+        onclick={() =>
+          setActive(() => {
+            return { ...scheme, profile: true };
+          })
+        }
       />
       <Item
         first={<BsCalendarWeekFill size={20} className="text-blue" />}
         second={<BsCalendarWeek size={20} className="text-gray" />}
         text="calender"
         clicked={active.calender}
-        onclick={()=>setActive(()=>{return({...scheme,calender:true})})}
+        onclick={() =>
+          setActive(() => {
+            return { ...scheme, calender: true };
+          })
+        }
       />
       <Item
         first={<RiBellFill size={20} className="text-blue" />}
         second={<RiBellLine size={20} className="text-gray" />}
         text="notification"
         clicked={active.notification}
-        onclick={()=>setActive(()=>{return({...scheme,notification:true})})}
+        onclick={() =>
+          setActive(() => {
+            return { ...scheme, notification: true };
+          })
+        }
       />
       <Item
         first={<AiTwotoneSetting size={20} className="text-blue" />}
         second={<AiOutlineSetting size={20} className="text-gray" />}
         text="setting"
         clicked={active.setting}
-        onclick={()=>setActive(()=>{return({...scheme,setting:true})})}
+        onclick={() =>
+          setActive(() => {
+            return { ...scheme, setting: true };
+          })
+        }
       />
+      <div className="pt-8">
+        <Item
+          first={<BsPower size={20} className="text-gray" />}
+          second={<BsPower size={20} className="text-gray" />}
+          clicked={false}
+          text="logout"
+        />
+      </div>
     </ul>
   );
 }
