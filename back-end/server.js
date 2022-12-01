b5ee660b6003f918e9283adbb37909466de6c049
nima <nima@gmail.com>
2022-12-01 17:09:03 +0330

    add cinfig file

--- back-end/server.js
+++ back-end/server.js
@@ -16,5 +16,5 @@ mongoose.connect(DB)
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port , ()=>{
-    console.log(`App is running on server ${port}`);
+    console.log(`App is running on server ${port}`); 
 })
